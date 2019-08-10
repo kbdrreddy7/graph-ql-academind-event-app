fb4dec0edb0f124e59ce725699afdd88c6b20a7a
Ravinder Reddy <42318559+kbdrreddy7@users.noreply.github.com>
2019-08-10 19:05:44 +0530

    removing secure data

--- back-end/env.js
+++ back-end/env.js
@@ -2,12 +2,6 @@
 process.env.TEST = "HELLO";
 process.env.APP_VERSION = "0.0.1";
 process.env.NODE_ENV = "DEVELOPMENT"; // PRODUCTION
-process.env.PORT = 8080;
+process.env.PORT = 5000;
 
-// secure data
-process.env.APP_URL = "http://www.uttarajavatraining.com:8066/DesktopApp";
-process.env.DATABASE_URL =
-  "mongodb://user1:user123@ds343127.mlab.com:43127/gql-video-app";
-// secret keys
 
-process.env.JWT_KEY = "jwt@123";
