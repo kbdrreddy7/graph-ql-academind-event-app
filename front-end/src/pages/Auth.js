e74b2101366ef9434fd438c93fa30f5d1b3df8a4
Ravinder Reddy Kottabad <kbdrreddy7@gmail.com>
2019-08-14 01:17:00 +0530

    basic signUp and login features are added(only sending requset to api)

--- front-end/src/pages/Auth.js
+++ front-end/src/pages/Auth.js
@@ -1,7 +1,97 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 class Auth extends Component {
+  state = {
+    isLogin: true
+  };
+  constructor(props) {
+    super(props);
+    this.emailEl = createRef();
+    this.passwordEl = createRef();
+  }
+
+  switchModeHandler = () => {
+    this.setState(previousState => {
+      return { isLogin: !previousState.isLogin };
+    });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+
+    let email = this.emailEl.current.value;
+    let password = this.passwordEl.current.value;
+
+    if (email.trim() === "" || password.trim() === "") return;
+
+    console.log(email, password);
+
+    let reqBody = {
+      query: `
+            query{
+                    login( email:"${email}", password:"${password}")
+                                                                  {
+                                                                    userId
+                                                                    token
+                                                                    tokenExpiration
+                                                                  }
+                    }`
+    };
+
+    if (!this.state.isLogin)
+      reqBody = {
+        query: `
+            mutation{
+                    createUser(userInput:{
+                                          email:"${email}",
+                                          password:"${password}"
+                                          })
+                                            {
+                                              _id
+                                              email
+                                            }
+                    }`
+      };
+
+    fetch(`http://localhost:5000/graphql`, {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+      .then(res => {
+        console.log(res);
+        if (res.status !== 200 && res.status !== 201) throw new Error("Failed");
+        return res.json();
+      })
+      .then(resData => {
+        console.log("resData", resData);
+      })
+      .catch(err => console.log("err ", err));
+  };
+
   render() {
-    return <h1>Auth Page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <h2 className="title">{this.state.isLogin ? "Login" : "SignUp"}</h2>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Swith to {this.state.isLogin ? "SignUp" : "Login"}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
